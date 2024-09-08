994f12dc6e051dc91983ebd7f96f3592e29701e8
unknown <akajeanyann@gmail.com>
2024-09-08 00:07:55 +0000

    update

--- server/boot/root.js
+++ server/boot/root.js
@@ -8,11 +8,11 @@
 module.exports = function (server) {
   var ds = server.dataSources.vasopportunitesAppDb;
   if (ds.connected) {
-     ds.automigrate();
+     ds.autoupdate();
     console.log("DB connected ✅")
   } else {
     ds.once('connected', function () {
-       ds.automigrate();
+       ds.autoupdate();
       console.log("DB connected ✅")
     });
   }
