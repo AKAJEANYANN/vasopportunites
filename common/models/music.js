9142598a19aa621d990d8051cbbc72b0f1903f15
unknown <akajeanyann@gmail.com>
2024-09-15 01:54:45 +0000

    update all

--- common/models/music.js
+++ common/models/music.js
@@ -2,7 +2,7 @@
 
 module.exports = function(Music) {
 
-    Music.countMusicByStatus = async function(idOperator, cb) {
+    Music.countMusicByStatus = async function(idOperator) {
         try {
           const currentDate = new Date();
           const startOfDay = new Date(currentDate.setHours(0, 0, 0, 0));  // Début de la journée
@@ -35,9 +35,9 @@ module.exports = function(Music) {
             }
           };
     
-          cb(null, result);
+          return (null, result);
         } catch (err) {
-          cb(err);
+          return (err);
         }
       };
     
