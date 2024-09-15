dbba809cc3580c1a87ae1f5afccf35bc1e2d8a90
unknown <akajeanyann@gmail.com>
2024-09-15 00:01:32 +0000

    update

--- common/models/music.js
+++ common/models/music.js
@@ -2,4 +2,49 @@
 
 module.exports = function(Music) {
 
+    Music.countMusicByStatus = async function(idOperator, cb) {
+        try {
+          const currentDate = new Date();
+          const startOfDay = new Date(currentDate.setHours(0, 0, 0, 0));  // Début de la journée
+          const endOfDay = new Date(currentDate.setHours(23, 59, 59, 999)); // Fin de la journée
+    
+          // Compter les clients en attente
+          const pendingMusic = await Music.count({ musicEtat: 'EN_ATTENTE', musicSupprime: false, operatorId: idOperator });
+    
+          // Compter les Music validés aujourd'hui
+          const validatedMusic = await Music.count({
+            musicEtat: 'VALIDEE',
+            musicActionDate: { between: [startOfDay, endOfDay] },
+            operatorId: idOperator
+          });
+    
+          // Compter les Music rejetés aujourd'hui
+          const rejectedMusic = await Music.count({
+            musicEtat: 'REFUSEE',
+            musicActionDate: { between: [startOfDay, endOfDay] },
+            operatorId: idOperator
+          });
+    
+          // Retourner les résultats
+          const result = {
+            date: currentDate,
+            pendingMusic: pendingMusic,
+            validatedMusicToday: validatedMusic,
+            rejectedMusicToday: rejectedMusic
+          };
+    
+          cb(null, result);
+        } catch (err) {
+          cb(err);
+        }
+      };
+    
+      // Exposer la méthode via une API REST
+      Music.remoteMethod('countMusicByStatus', {
+        accepts: 
+            { arg: 'idOperator', type: 'string'},
+        http: { path: '/count-music-by-status', verb: 'get' },
+        returns: { arg: 'data', type: 'object' }
+      });
+
 };
