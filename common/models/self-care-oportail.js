fa325bfc5c905b7361a11d3faeaeaff483a32086
unknown <akajeanyann@gmail.com>
2024-11-18 11:44:42 -0500

    update

--- common/models/self-care-oportail.js
+++ common/models/self-care-oportail.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const numberService = require("../serviceOportail/selfcare/number.service");
+
 module.exports = function(Selfcareoportail) {
 
+    numberService(Selfcareoportail)
 };
