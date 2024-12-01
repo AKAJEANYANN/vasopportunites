b9d70596fc6e69c2b16e1ae6273d36a72466e287
unknown <akajeanyann@gmail.com>
2024-12-01 18:20:54 -0500

    update

--- common/models/selfcare-has-media-oportail.js
+++ common/models/selfcare-has-media-oportail.js
@@ -1,5 +1,48 @@
 'use strict';
 
 module.exports = function(Selfcarehasmediaoportail) {
+    Selfcarehasmediaoportail.searching = function (req, cb) {
+
+        var idMediaOportail = req.body.mediaOportailId
+        var idSelfCareOportail = req.body.selfCareOportailId
+        var Vlike = req.body.like
+
+        Selfcarehasmediaoportail.findOne({
+            where:{
+                mediaOportailId: idMediaOportail,
+                selfCareOportailId: idSelfCareOportail
+            }
+        },(err, selfhasmed)=>{
+
+            if(selfhasmed != null){
+
+                selfhasmed.updateAttributes({
+                    like: Vlike
+                },(err, selfhasm)=>{
+                    if(err)cb(err, null)
+                        else cb(null, selfhasm)
+                })
+            }else{
+                Selfcarehasmediaoportail.create({
+                    mediaOportailId: idMediaOportail,
+                    selfCareOportailId: idSelfCareOportail,
+                    like: Vlike
+                },(err, selfmed)=>{
+                    if(err)cb(err, null)
+                        else cb(null, selfmed)
+                })
+            }
+        })
+        
+    }
+    
+      
+    Selfcarehasmediaoportail.remoteMethod('searching', {
+        accepts: [
+            { arg: 'req', type: 'object', 'http': {source: 'req'}},
+        ],
+        http: { path: '/searching', verb: 'post' },
+        returns: { arg: 'data', type: 'object' }
+      });
 
 };
